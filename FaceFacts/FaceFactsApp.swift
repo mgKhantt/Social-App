634f33b2ff6e1b9e7329ff40b60eb25420ea754e
mgKhantt <khantphonenaing156@gmail.com>
2024-12-25 19:41:18 +0630

    Initial Commit

---
+++ FaceFacts/FaceFactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceFactsApp.swift
+//  FaceFacts
+//
+//  Created by Khant Phone Naing  on 25/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct FaceFactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FaceFactsTests/FaceFactsTests.swift
@@ -0,0 +1,17 @@
+//
+//  FaceFactsTests.swift
+//  FaceFactsTests
+//
+//  Created by Khant Phone Naing  on 25/12/2024.
+//
+
+import Testing
+@testable import FaceFacts
+
+struct FaceFactsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

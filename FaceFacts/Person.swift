559ae049381a771544b3651350dc79fb28fe3ae2
mgKhantt <khantphonenaing156@gmail.com>
2024-12-26 20:11:10 +0630

    Adding Person Class

---
+++ FaceFacts/Person.swift
@@ -0,0 +1,20 @@
+//
+//  Person.swift
+//  FaceFacts
+//
+//  Created by Khant Phone Naing  on 26/12/2024.
+//
+
+import Foundation
+
+class Person {
+    var name: String
+    var emailAddress: String
+    var details: String
+    
+    init(name: String, emailAddress: String, details: String) {
+        self.name = name
+        self.emailAddress = emailAddress
+        self.details = details
+    }
+}
